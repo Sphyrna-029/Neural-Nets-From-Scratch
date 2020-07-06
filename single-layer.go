64ec240d733fa9160cef7b14a193cf8a7d3b3fe9
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2020-07-05 23:25:51 -0400

    Update single-layer.go

--- single-layer.go
+++ single-layer.go
@@ -11,10 +11,12 @@ func main() {
 
     var bias1, bias2, bias3 float64 = 2, 3, 0.5
 
-    
+	//                                           {               A single neuron           }
+	//Inputs times the weights plus the bias (x = i1 * w1 + i2 * w2 + i3 * w3 + i4 * w4 + b).
+	//This is a single layer comprising of 3 neurons, 4 inputs, 4 weights specific to each neuron, and 3 bias' for each neuron
+
     output := [3]float64{inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
                          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
                          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3}
 
     fmt.Println(output)
-}
